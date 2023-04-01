47307b85ff61d1b9196c17a072b5797d48429648
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2023-04-02 00:52:14 +0200

    hundel intro app

--- index.js
+++ index.js
@@ -1,41 +1,22 @@
-var createError = require("http-errors");
-// require("./Config");
-var express = require("express");
-var logger = require("morgan");
-const path = require("path");
-const cors = require("cors");
-const connect = require("./configuration/conctionDb");
-const { Router } = require("express");
+ const express= require('express');
 
+ const dotenv=require('dotenv');
+ dotenv.config({path:"config.env"})
 const app = express();
-// connect();
+const cors = require('cors');
+const port =process.env.PORT 
 
-app.use(logger("dev"));
-app.use(express.static(path.join(__dirname, "public")));
-// Allow json data
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(cors())
+app.use(express.urlencoded({extended:true}));
+app.use(cors());
 
 
 
 
-app.use(function (req, res, next) {
-    next(createError(404));
-  });
-  
-  // error handler
-  app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get("env") === "development" ? err : {};
-  
-    // render the error message
-    res.status(err.status || 500);
-    res.json({
-      message:" error  server",
-  
-    });
-  });
-  module.exports=Router()
-  module.exports = app;
+app.listen(port,()=>{
+    console.log("the server is listening")
+});
+
+app.get('/',(req, res,next)=>{
+    res.send("the server listening")
+})
