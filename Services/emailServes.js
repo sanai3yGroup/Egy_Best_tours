5e5d9064b163383b8e799b5be3979471e7ae1859
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2023-04-08 03:08:19 +0200

    send message done

--- Services/emailServes.js
+++ Services/emailServes.js
@@ -1,5 +1,9 @@
 const Mail = require("../Models/Mailes");
 const ApiError = require("../Utilites/ApiError");
+
+const sendMessageTouser= require("../Utilites/sendMail");
+
+
 exports.creatMail = async (req, res, next) => {
   try {
     const mail= await Mail.create(req.body);
@@ -13,16 +17,19 @@ exports.creatMail = async (req, res, next) => {
   }
 };
 
-exports.getMails = async (rea, res, next) => {
+exports.getMails = async (req, res, next) => {
     try{
+      const data= req.body;
         const mails= await Mail.find();
-        if(!mails) return next(ApiError(400,'error getting data'));
+        if(!mails ) return next(ApiError(400,'error not found mails'));
+        mails.map((mail) =>{
+          sendMessageTouser(mail.email,data);
+        })
            res.json({
             statusCode:200,
-            data:mails
-            
+            message:"the message send successfully",
            })
     }catch(err){
-        next( new ApiEorr(500,'the server occurred an error'))
+        next( new ApiError(500,'the server occurred an error'))
     }
 };
