e269d81bec3b0d38bbf36437d8872cdb785904fe
bassamnegm <bassamnegm@gmail.com>
2023-04-27 23:38:21 +0200

    add filter

--- Services/packageService.js
+++ Services/packageService.js
@@ -151,3 +151,28 @@
       new ApiError(500, err.message)
       }
     }
+
+    exports.getFilterPackages = async (req, res, next) => {
+      try {
+        const {pageNumber=1,...query}=req.query
+       // console.log(query);
+        const limit = 10;
+        const skip = (pageNumber - 1) * limit;
+        const packages = await Package.find(query)
+          .populate("location")
+          .populate("category")
+          .skip(skip)
+          .limit(limit);
+        const numOfPage = Math.ceil((await Package.find().count()) / limit);
+        if (packages.length <= 0)
+          return next(new ApiError(404, "not found any trips"));
+        res.json({
+          statusCode: 200,
+          numOfPage: numOfPage,
+          message: "you have all packages successfully",
+          data: packages,
+        });
+      } catch (err) {
+        next(new ApiError(500, err));
+      }
+    };
