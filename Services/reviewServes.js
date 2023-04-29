d04d1ac123e2313f24c744c7d5f5a2f233ee5095
bassamnegm <bassamnegm@gmail.com>
2023-04-29 20:34:39 +0300

    add rate

--- Services/reviewServes.js
+++ Services/reviewServes.js
@@ -6,13 +6,12 @@ exports.createReview = async (req, res, next) => {
   try {
     const review = await new Review(req.body);
     if (!review) return next(new ApiError(403, "the review is not created "));
-    console.log(review)
+    
     const data=await Package.aggregate([{$match:{_id:review.packageId}},
     {
      $unwind:'$reviewes' 
     },{$group:{_id:null,rate:{$avg:'$reviewes.rate'}}}])
-    console.log(data);
-   
+    
     const packageId = await Package.findByIdAndUpdate(
       { _id: review.packageId },
       {
