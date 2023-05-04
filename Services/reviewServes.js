7a93eec2d047bf56f057a1a0e18facfea3db3aa9
bassamnegm <bassamnegm@gmail.com>
2023-05-04 21:02:12 +0300

    fix rate

--- Services/reviewServes.js
+++ Services/reviewServes.js
@@ -6,25 +6,28 @@ exports.createReview = async (req, res, next) => {
   try {
     const review = await new Review(req.body);
     if (!review) return next(new ApiError(403, "the review is not created "));
-    
-    const data=await Package.aggregate([{$match:{_id:review.packageId}},
-    {
-     $unwind:'$reviewes' 
-    },{$group:{_id:null,rate:{$avg:'$reviewes.rate'}}}])
-    
     const packageId = await Package.findByIdAndUpdate(
       { _id: review.packageId },
       {
         $push: { reviewes: review},
-        $set: {totalrate: data[0].rate}
+        // $set: {totalrate: data[0].rate}
       }
     );
+    const data=await Package.aggregate([{$match:{_id:review.packageId}},
+    {
+     $unwind:'$reviewes' 
+    },{$group:{_id:null,rate:{$avg:'$reviewes.rate'}}}])
+    packageId.totalrate=data[0].rate;
+    console.log(data[0].rate);
+    packageId.save();
+   
     res.json({
       statusCode: 200,
       message: "the review is created successfully",
       dat: review,
     });
   } catch (err) {
+    console.log(err);
     next(new ApiError(500, "the server returned an error"));
   }
 };
