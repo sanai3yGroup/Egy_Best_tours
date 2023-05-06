50cb7e772f24bf93cd55ab0758bc459d50de99f6
ALIABDO111 <Aboalhassanabdo769@gmail.com>
2023-05-06 17:59:32 +0300

    and try to deploy thgat module

--- Services/blogService.js
+++ Services/blogService.js
@@ -47,7 +47,7 @@ exports.getBlog = async (req, res, next) => {
 exports.getAllBlog = async (req, res, next) => {
   try {
     const pageNumber = req.params.pageNumber || 1;
-    const limit = 15;
+    const limit = 10;
     const skip = (pageNumber - 1) * limit;
     const blogs = await Blog.find().skip(skip).limit(limit);
     const numOfPage = Math.ceil((await Blog.find().count()) / limit);
